9de99e151293c859a4240ddd9b3c68d22bfba19a
Lynnsay Piper <lynnsaymay.piper@outlook.com>
2017-06-27 23:06:11 +0100

    Added initial add processor and failing test cases

---
+++ SumOfNumbers.Tests/Processors/AddProcessorTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace SumOfNumbers.Tests.Processors
+{
+    [TestFixture]
+    public class AddProcessorTests
+    {
+        [TestCase(1, 1, ExpectedResult = 2)]
+        [TestCase(10, 10, ExpectedResult = 20)]
+        [TestCase(200, 200, ExpectedResult = 400)]
+        public long AddTwoIntegers(long integerOne, long integerTwo)
+        {
+            return 0;
+        }
+    }
+}
---
+++ SumOfNumbers/Interfaces/IAddProcessor.cs
@@ -0,0 +1,7 @@
+﻿namespace SumOfNumbers.Interfaces
+{
+    public interface IAddProcessor
+    {
+        long Add(long integerOne, long integerTwo);
+    }
+}
