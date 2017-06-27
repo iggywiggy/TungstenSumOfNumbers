178f89e253df13d39bec9e2335c1cf5b4d1d06db
Lynnsay Piper <lynnsaymay.piper@outlook.com>
2017-06-27 23:09:35 +0100

    Made tests passed initially

--- SumOfNumbers.Tests/Processors/AddProcessorTests.cs
+++ SumOfNumbers.Tests/Processors/AddProcessorTests.cs
@@ -1,16 +1,33 @@
 ﻿using NUnit.Framework;
+using SumOfNumbers.Interfaces;
 
 namespace SumOfNumbers.Tests.Processors
 {
     [TestFixture]
     public class AddProcessorTests
     {
+        private IAddProcessor _addProcessor;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _addProcessor = new AddProcess();
+        }
+
         [TestCase(1, 1, ExpectedResult = 2)]
         [TestCase(10, 10, ExpectedResult = 20)]
         [TestCase(200, 200, ExpectedResult = 400)]
         public long AddTwoIntegers(long integerOne, long integerTwo)
         {
-            return 0;
+            return _addProcessor.Add(integerOne, integerTwo);
+        }
+    }
+
+    public class AddProcess : IAddProcessor
+    {
+        public long Add(long integerOne, long integerTwo)
+        {
+            return integerOne + integerTwo;
         }
     }
 }
