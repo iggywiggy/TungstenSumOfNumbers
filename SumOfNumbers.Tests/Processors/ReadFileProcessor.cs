96ff4142ad999cfc701f7d3a5a380f1edc35c3f6
Lynnsay Piper <lynnsaymay.piper@outlook.com>
2017-06-28 12:00:04 +0100

    Added ReadFileProcessor

---
+++ SumOfNumbers.Tests/Processors/ReadFileProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using SumOfNumbers.Interfaces;
+
+namespace SumOfNumbers.Tests.Processors
+{
+    [TestFixture]
+    public class ReadFileProcessor
+    {
+        private IReadFileProcessor _readFileProcessor;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _readFileProcessor = new Classes.Processors.ReadFileProcessor();
+        }
+
+
+        [Test]
+        public void Read_EmptyString_Path_Parameter()
+        {
+            Assert.Throws<ArgumentNullException>(() => _readFileProcessor.Read(""));
+        }
+
+        [Test]
+        public void Read_Null_Path_Parameter()
+        {
+            Assert.Throws<ArgumentNullException>(() => _readFileProcessor.Read(null));
+        }
+
+        [Test]
+        public void Read_Return_EmptyString_File_DoesntExist()
+        {
+            Assert.That(() => _readFileProcessor.Read("file") == string.Empty);
+        }
+
+        [Test]
+        public void ReadFileProcessor_IsInstance_Of_IReadFileProcessor()
+        {
+            Assert.IsInstanceOf<IReadFileProcessor>(_readFileProcessor);
+        }
+    }
+}
