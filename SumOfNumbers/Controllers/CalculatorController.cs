b69f621d6e02eb259fd9fdd77273d767de40bb67
Lynnsay Piper <lynnsaymay.piper@outlook.com>
2017-06-28 11:26:28 +0100

    Added config for logging

--- SumOfNumbers/Controllers/CalculatorController.cs
+++ SumOfNumbers/Controllers/CalculatorController.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using System.Web.Http.ModelBinding;
+using log4net;
 using SumOfNumbers.Infastructure;
 using SumOfNumbers.Interfaces;
 
@@ -8,10 +10,13 @@ namespace SumOfNumbers.Controllers
     public class CalculatorController : ApiController
     {
         private readonly ICommandWithResult<long> _command;
+        private readonly ILog _log;
 
-        public CalculatorController(ICommandWithResult<long> command)
+        public CalculatorController(ICommandWithResult<long> commander, ILogManager logManager)
         {
-            _command = command;
+            _command = commander ?? throw new ArgumentNullException(nameof(commander));
+            _log = logManager?.GetLog(typeof(CalculatorController)) ??
+                   throw new ArgumentNullException(nameof(logManager));
         }
 
         [HttpPost]
@@ -25,6 +30,9 @@ namespace SumOfNumbers.Controllers
             Utils.ConvertStringToInt(integerTwo, out long valueTwo);
 
             _command.Execute(new object[] {valueOne, valueTwo});
+
+            _log.Info(
+                $"DateTime: {DateTime.Now}, ValueOne: {valueOne}, ValueTwo: {valueTwo}, Result: {_command.Result}");
 
             return Ok(_command.Result);
         }
